dbf3bc02dd6dcc422c113dece2cbc0ae920d643e
nouse4it <github@schlueter-online.net>
2022-09-19 15:10:58 +0200

    added comments for documentation purposes

--- get_guest_user_ISE_API.py
+++ get_guest_user_ISE_API.py
@@ -32,6 +32,7 @@ api_pw = getpass.getpass(prompt='Enter API Password: ')
 
 
 def get_user():
+    # Definition of the URL that will be queried and the neccessary API settings and credentials
     url = 'https://{}:9060/ers/config/guestuser/'.format(ise_ip)
     headers = {'ACCEPT': 'application/json',
                'content-type': 'application/json'}
@@ -40,10 +41,15 @@ def get_user():
     myjson = req.text
     parsed_json = (json.loads(myjson))
     users = []
+    # Using CSV Writer to export defined fields from 'parsed_json' to a csv-file
+    # Also rows and fields of csv are writte and configured
     with open('export_guestuser_ise.csv', 'w') as f:
         writer = csv.writer(f)
         writer.writerow(["Username", "First Name", "Last Name", "Company",
                         "Enabled", "Valid Days", "From Date", "To Date", "Location"])
+
+        # this outcommented block is only needed when there are more then 100 results, because ISE starts paging from there. Uncomment if needed
+
         # for i in range(1, pages+1):
         # url_page = 'https://{}:9060/ers/config/guestuser?size=100&page={}'.format(
         # ise_ip, i)
@@ -51,8 +57,12 @@ def get_user():
         # api_user, api_pw), verify=False)
         # myjson = req_page.text
         # parsed_json = (json.loads(myjson))
+
+        # Parse through the JSON and get the ID of every Guest-User
         for user in parsed_json['SearchResult']['resources']:
             users.append(user['id'])
+
+        # Get wanted information for every ID that was extraced previously
         for id in users:
             url = 'https://{}:9060/ers/config/guestuser/{}'.format(ise_ip, id)
             req = requests.get(url, headers=headers, auth=(
@@ -68,6 +78,8 @@ def get_user():
             fromdate = parsed_json['GuestUser']['guestAccessInfo']['fromDate']
             todate = parsed_json['GuestUser']['guestAccessInfo']['toDate']
             location = parsed_json['GuestUser']['guestAccessInfo']['location']
+
+            # Write informations to csv-file
             row = [username, firstname, lastname, company,
                    enabled, validdays, fromdate, todate, location]
             writer.writerow(row)
